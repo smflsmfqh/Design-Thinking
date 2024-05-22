5f7c5342816e21e2a619c33bd4f271a5e9f344f4
smflsmfqh <150312623+smflsmfqh@users.noreply.github.com>
2024-05-23 05:30:48 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'pages/pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smoke Spot',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashPage(),
+    );
+  }
+}
